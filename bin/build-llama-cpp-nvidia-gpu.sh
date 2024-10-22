22dc66d5f044ca31fd230209123fdc6973b0f142
davidrpugh <david.pugh@kaust.edu.sa>
2024-10-22 09:51:26 +0300

    Use CONDA_PREFIX variable.

--- bin/build-llama-cpp-nvidia-gpu.sh
+++ bin/build-llama-cpp-nvidia-gpu.sh
@@ -12,18 +12,18 @@ if [ ! -d "$SRC_DIR" ]; then https://github.com/ggerganov/llama.cpp.git "$SRC_DI
 BUILD_DIR="$PROJECT_DIR"/build/llama-cpp
 if [ -d "$BUILD_DIR" ]; then rm -rf "$BUILD_DIR"; fi
 cmake -S "$SRC_DIR" -B "$BUILD_DIR" \
-    -DCMAKE_INSTALL_PREFIX="$PWD"/env    `# install binaries into conda environment` \
-    -DCMAKE_INSTALL_RPATH="$PWD"/env/lib `# insure that dyanmic libs can be found at runtime` \
-    -DGGML_LLAMAFILE=OFF                 `# support for Q4_0_4_4 quantization` \
-    -DGGML_CUDA=ON                       `# support for NVIDIA GPU accleration` \
-    -DGGML_BLAS=ON                       `# support for CPU accleration using BLAS` \
+    -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX"    `# install binaries into conda environment` \
+    -DCMAKE_INSTALL_RPATH="$CONDA_PREFIX"/lib `# insure that dyanmic libs can be found at runtime` \
+    -DGGML_LLAMAFILE=OFF                      `# support for Q4_0_4_4 quantization` \
+    -DGGML_CUDA=ON                            `# support for NVIDIA GPU accleration` \
+    -DGGML_BLAS=ON                            `# support for CPU accleration using BLAS` \
     -DGGML_BLAS_VENDOR=OpenBLAS
 
 # build llama.cpp
 cmake --build "$BUILD_DIR" --config Release
 
 # install the compiled binaries into the conda environment
-cmake --install "$BUILD_DIR" --prefix "$PWD"/env
+cmake --install "$BUILD_DIR" --prefix "$CONDA_PREFIX"
 
 # remove the source directory
 rm -rf "$SRC_DIR"
